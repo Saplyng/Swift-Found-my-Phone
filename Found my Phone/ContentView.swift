038c08059b2adea1578bba1cd0bbd985081a9f81
Jeremy Stein <saplyng@gmail.com>
2020-10-12 00:53:31 -0500

    Update ContentView.swift
    content content content!

--- Found my Phone/ContentView.swift
+++ Found my Phone/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var yesShowing = false
+    @State private var noShowing = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("Hey, did you find this phone randomly?")
+                .padding()
+            Button("Yes"){
+                self.yesShowing.toggle()
+            }.padding()
+            .sheet(isPresented: $yesShowing){
+                Text("maybe give it back to the guy who dropped it, just ship it to his address: 123 calender lane")
+            }
+            Button("No"){
+                self.noShowing.toggle()
+            }.padding()
+            .sheet(isPresented: $noShowing){
+                Text("hey uhh, why are you looking at this me? go look at reddit or something")
+            }
+        }
     }
 }
 
