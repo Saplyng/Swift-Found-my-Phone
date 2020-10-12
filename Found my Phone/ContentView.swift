0b0df6990928e496ac92f4c7430e35bc88196082
Jeremy Stein <saplyng@gmail.com>
2020-10-12 00:39:12 -0500

    Initial Commit

---
+++ Found my Phone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Found my Phone
+//
+//  Created by student on 10/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Found my Phone/Found_my_PhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Found_my_PhoneApp.swift
+//  Found my Phone
+//
+//  Created by student on 10/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Found_my_PhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
